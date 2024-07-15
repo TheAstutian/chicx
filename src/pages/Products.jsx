cf2cc4caa9d8bdbfbe9e3aeadb7652961059465e
TheAstutian <speakingatoms@gmail.com>
2024-07-15 17:04:11 +0100

    search feature fe

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -21,6 +21,8 @@ const Products = () => {
   const [store,setStore]= useState(null)
   const [currentStore, setCurrentStore] = useState(null); 
   const [displayCategory, setDisplayCategory]= useState('');
+  const[activateSearch, setActivateSearch]= useState(false)
+  const [searchQuery, setSearchQuery] = useState('')
   
 
   const [currentPage, setCurrentPage] = useState(1);
@@ -31,13 +33,13 @@ const Products = () => {
         
         try{
           
-          const fetchStore = await axios.get(`${API_URL}/store?page=${currentPage}&category=${displayCategory}`)
+          const fetchStore = await axios.get(`${API_URL}/store?page=${currentPage}&searchquery=${searchQuery}&category=${displayCategory}`)
           if(fetchStore){ 
             
             const TotalPages = Math.ceil(fetchStore.data[0].totalCount[0].count/rowsPerPage)
             
            /* console.log(fetchStore.data[0].totalData)*/
-           
+           console.log(searchQuery)
             setTotalPages(TotalPages)
             setCurrentStore(fetchStore.data[0].totalData)
           }
@@ -49,22 +51,26 @@ const Products = () => {
       
       window.scrollTo(0,0)
       return
-    }, [currentPage, displayCategory])
+    }, [currentPage, displayCategory, activateSearch])
 
-    const changeStore = (store)=>{
-      if(store){
-        setCurrentStore(store)
+    const changeCategory = (category)=>{
+    setDisplayCategory(category)
+    setCurrentPage(1)
+    }
+
+    const handleKeyPress = (event)=>{
+      
+      if(event.key==='Enter'){
+        onSearch(event)
       }
     }
-
-     
+     const onSearch = async (e)=>{
+        e.preventDefault()
+        console.log(searchQuery)
+        setActivateSearch(!activateSearch)
+        
+     }
     
-    const paginate = (array,pageNumber,itemsPerPage) =>{
-      const startIndex = (pageNumber-1)*itemsPerPage;
-      const endIndex = startIndex + itemsPerPage;
-
-      return array.slice(startIndex, endIndex); 
-    }
   
     //var activeArray = paginate(store,currentPage,10);
    
@@ -117,13 +123,13 @@ const Products = () => {
                 
               <div tabIndex={0} role="button" className='btn btn-sm btn-outline bg-gray hover:bg-tertiary text-tertiary hover:text-white m-1'>Shop by category <IoIosArrowDown /> </div>
               <ul tabIndex={0} className='dropdown-content menu bg-tertiary rounded-box z-[1] w-52 p-2 ml-1 shadow'>
-                <li onClick={()=>{setDisplayCategory('') }} className='text-zinc'><a>All Categories</a></li>
-                <li onClick={()=>setDisplayCategory('Babies & Kids')} className='text-white'><a>Babies & Kids</a></li>
-                <li onClick={()=>setDisplayCategory('Kitchen Utensils & Home Essentials')} className='text-white'><a> Kitchen Utensils & Home Essentials</a></li>
-                <li onClick={()=>setDisplayCategory('Gifts & Souvenirs')}className='text-white'><a> Gifts & Souvenirs</a></li>
-                <li onClick={()=>setDisplayCategory('Decors ')}className='text-white'><a> Decors</a></li>
-                <li onClick={()=>setDisplayCategory('Exercise & Fitness Supplies')} className='text-white'><a> Exercise & Fitness Supplies </a></li>
-                <li onClick={()=>setDisplayCategory('Resin Materials & Tools')}className='text-white'><a> Resin Materials & Tools</a></li>
+                <li onClick={()=>{changeCategory('') }} className='text-zinc'><span>All Categories</span></li>
+                <li onClick={()=>changeCategory('Babies & Kids')} className='text-white'><span>Babies & Kids</span></li>
+                <li onClick={()=>changeCategory('Kitchen Utensils & Home Essentials')} className='text-white'><span> Kitchen Utensils & Home Essentials</span></li>
+                <li onClick={()=>changeCategory('Gifts & Souvenirs')}className='text-white'><span> Gifts & Souvenirs</span></li>
+                <li onClick={()=>changeCategory('Decors ')}className='text-white'><span> Decors</span></li>
+                <li onClick={()=>changeCategory('Exercise & Fitness Supplies')} className='text-white'><span> Exercise & Fitness Supplies </span></li>
+                <li onClick={()=>changeCategory('Resin Materials & Tools')}className='text-white'><span> Resin Materials & Tools</span></li>
             
               </ul>
 
@@ -131,47 +137,44 @@ const Products = () => {
             </div>
             <div className='px-3 py-2 flex flex-row h-12 md:mr-10 md:pr-10 relative'>
             <input
-    type="search" 
-    className="peer block min-h-[auto] w-full  rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline outline-1 outline-tertiary transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-    placeholder="Search"
-    aria-label="Search"
-    id="exampleFormControlInput"
-    aria-describedby="basic-addon1" />     
-  <label
-    for="exampleFormControlInput"
-    className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.47rem] pl-1 leading-[1.8] text-neutral-500 italic transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-tertiary"
-    >Search
-  </label>
-  <button
-    className="relative z-[2] -ms-0.5 flex items-center rounded-e bg-tertiary px-3  text-xs font-medium uppercase leading-normal text-gray shadow-tertiary transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-    type="button"
-    id="button-addon1" 
-    data-twe-ripple-init
-    data-twe-ripple-color="light"> 
-    <span className="[&>svg]:h-5 [&>svg]:w-5">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor">
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-      </svg>
-    </span>
-  </button>
+                  type="search" 
+                  className="peer block min-h-[auto] w-full  rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline outline-1 outline-tertiary transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+                  placeholder="Search"
+                  onChange={(e)=>{setSearchQuery(e.target.value)}}
+                  onKeyDown={(e)=>{handleKeyPress(e)}}
+                   />     
+                <label
+                  htmlFor="exampleFormControlInput"
+                  className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.47rem] pl-1 leading-[1.8] text-neutral-500 italic transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-tertiary"
+                  >Search
+                </label>
+                <button onClick={onSearch}
+                  className="relative z-[2] -ms-0.5 flex items-center rounded-e bg-tertiary px-3  text-xs font-medium uppercase leading-normal text-gray shadow-tertiary transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+                  type="button"> 
+                  <span className="[&>svg]:h-5 [&>svg]:w-5">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor">
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                  </span>
+                </button>
 
             </div>
           </div>
           
-          <hr className=' h-px my-2 bg-gray-400 border-0'/>
+          <hr key="2" className=' h-px my-2 bg-gray-400 border-0'/>
           
             {currentStore? <>
               
                 {currentStore.map((item)=>(<>
-                  <div style={{"width":"100%"}} className='p-2  pl-1 md:pl-20 pt-5 m-2 mb-10 md:mb-5 h-32 md:h-26 flex flex-row'> 
+                  <div style={{"width":"100%"}}  key={`${item._id}`} className='p-2  pl-1 md:pl-20 pt-5 m-2 mb-10 md:mb-5 h-32 md:h-26 flex flex-row'> 
                    <div className=' flex-none w-32 '> 
                          <Link to={`/products/${item._id}`}><img className='h-28 md:h-24 m-auto hover:opacity-70' src={item.imageUrl} /></Link>
                     </div>
@@ -197,19 +200,19 @@ const Products = () => {
 {/*PAGINATION HERE */}
                 <div className='p-3 my-5 mr-3 relative'>
                 <nav className=" absolute bottom-0 right-0 inline-flex items-center p-1 rounded bg-gray space-x-2" >
-                  <a onClick={()=> setCurrentPage(1)} className="p-1 rounded border cursor-pointer text-tertiary bg-white hover:text-white hover:bg-tertiary hover:border-black">
+                  <span onClick={()=> setCurrentPage(1)} className="p-1 rounded border cursor-pointer text-tertiary bg-white hover:text-white hover:bg-tertiary hover:border-black">
                     <RxDoubleArrowLeft/>
-                  </a>
-                  <a onClick={() => setCurrentPage(currentPage => Math.max(currentPage - 1, 1))} disabled={currentPage === 1} className="p-1 rounded border text-tertiary bg-white hover:text-white cursor-pointer hover:bg-tertiary hover:border-white" >
+                  </span>
+                  <span onClick={() => setCurrentPage(currentPage => Math.max(currentPage - 1, 1))} disabled={currentPage === 1} className="p-1 rounded border text-tertiary bg-white hover:text-white cursor-pointer hover:bg-tertiary hover:border-white" >
                   <IoIosArrowBack />
-                  </a>
+                  </span>
                   <span className="text-tertiary">{` Page ${currentPage} of ${totalPages} `}</span>
-                  <a onClick={() => setCurrentPage(currentPage => Math.min(currentPage + 1, totalPages))} disabled={currentPage === totalPages} className="p-1 rounded border cursor-pointer text-tertiary bg-white hover:text-white hover:bg-tertiary hover:border-black">
+                  <span onClick={() => setCurrentPage(currentPage => Math.min(currentPage + 1, totalPages))} disabled={currentPage === totalPages} className="p-1 rounded border cursor-pointer text-tertiary bg-white hover:text-white hover:bg-tertiary hover:border-black">
                   <IoIosArrowForward />
-                  </a>
-                  <a onClick={()=> setCurrentPage(totalPages)} className="p-1 rounded border cursor-pointer text-tertiary bg-white hover:text-white hover:bg-tertiary hover:border-black">
+                  </span>
+                  <span onClick={()=> setCurrentPage(totalPages)} className="p-1 rounded border cursor-pointer text-tertiary bg-white hover:text-white hover:bg-tertiary hover:border-black">
                   <RxDoubleArrowRight/>
-                  </a>
+                  </span>
                 </nav>
                 </div>
 
