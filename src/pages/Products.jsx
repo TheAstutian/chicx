c9d2082f2ac660b7dc7126f5dd29521fc695aac9
TheAstutian <speakingatoms@gmail.com>
2024-07-22 21:21:43 +0100

    bug fixes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -138,7 +138,7 @@ const Products = () => {
             <div className='ml-5 md:px-3 mt-1 py-2 flex flex-row h-12 md:mr-10 md:pr-10 relative'>
             <input
                   type="search" 
-                  className="peer block min-h-[auto] w-3/4 md:w-5/6  rounded border-0 bg-transparent  md:px-3 py-[0.32rem] leading-[1.6] outline outline-1 outline-tertiary transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+                  className="peer block min-h-[auto] w-3/4 md:w-full  rounded border-0 bg-transparent  md:px-3 py-[0.32rem] leading-[1.6] outline outline-1 outline-tertiary transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
                   placeholder="Search"
                   onChange={(e)=>{setSearchQuery(e.target.value)}}
                   onKeyDown={(e)=>{handleKeyPress(e)}}
