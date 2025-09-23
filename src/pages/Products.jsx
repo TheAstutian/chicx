7c2e2e9f0b6d4a8c0310ee83246c00165c246b4d
Emmanuel <emmanuel@Mac.Home>
2025-09-23 20:52:29 +0100

    ...

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -109,7 +109,7 @@ const Products = () => {
 
 
   return (
-    <div className=' scroll-smooth' >
+    <div className=' bg-[#e5e5e5] scroll-smooth' >
       {/*ADMIN SECTION*/}
         { currentUser? ( currentUser.type==='admin'?(
         <div className='flex items-right justify-right bg-white py-2.5 pt-4 px-5 pl-8'> 
