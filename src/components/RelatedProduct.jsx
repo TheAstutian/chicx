4b91ca184da8c0e9f09f220ccdbd0610572573f7
Emmanuel <emmanuel@Mac.Home>
2026-04-10 18:58:08 +0100

    upgraded relateditmes component to reflect new changes

--- src/components/RelatedProduct.jsx
+++ src/components/RelatedProduct.jsx
@@ -1,12 +1,16 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { API_URL } from '../App';
 import { Link } from 'react-router-dom';
 import { capitalizeTitle } from '../pages/Products';
+import { CartContext } from 'ContextStore';
+import {MdAddShoppingCart} from "react-icons/md";
 
 const RelatedProduct = (props) =>{
+const{ addToCart}= useContext(CartContext)
 const [relatedItems, setRelatedItems] = useState(null)
 const [otherItems, setOtherItems] = useState(null)
+  const [notification, setNotification] = useState({show:false, message:""})
 const id = props.id; 
 
 useEffect (()=>{
@@ -22,6 +26,60 @@ useEffect (()=>{
 }, [id]) 
 
 
+const showNotification=(message)=>{
+  setNotification({show:true, message})
+  
+  setTimeout(()=>{
+    setNotification({show:false, message:''})
+  }, 3000)
+    }
+
+    const handleAddtoCart = ({item}) =>{
+      addToCart(item)
+      showNotification(`Item added to your cart!`)
+    }
+
+
+ const ProductCard = ({item}) =>{
+
+        
+      return (
+        <>
+        <div   key={`${item._id}`} className='flex flex-col my-3 pb-3 md:pb-5 bg-white border border-gray-300 w-full max-w-180px md:max-w-[250px] h-68 '> 
+                 
+                   <div className=' w-full relative aspect-[4/5] overflow-hidden bg-slate-100 '> 
+                         <Link to={`/products/${item._id}`} ><img className='h-full w-full object-cover transition-transform duration-500 group-hover:scale-110 hover:opacity-70' src={item.imageUrl} />
+                         <span className="absolute top-0 left-0 m-2 bg-red-600 px-2 text-center text-xs font-normal text-white"> {item.popular? "Popular": ""} </span>
+                         {/*<span className="absolute top-5 left-0 m-2 rounded-full bg-red-600 px-2 text-center text-xs font-normal text-white"> {item.deal? "Good deal": ""} </span>*/}
+                         </Link>
+                    </div>
+
+
+                    <div className='w-full px-2 md:px-3'>
+                    <Link to={`/products/${item._id}`}> 
+                       <div className='h-12  md:py-2'><p className='text-gray-800 mt-2 md:mt-0 text-sm md:text-base line-clamp-2 md:line-clamp-2 md:py-1 mx-auto'>{capitalizeTitle(item.name)}</p></div>
+                        </Link>   
+                       <div className='flex flex-row justify-between md:px-2'>
+                          <div className='flex flex-col'> <span className='text-tertiary font-bold text-sm md:text-base '>₦{Intl.NumberFormat("en-US").format(item.sellingPrice)}     </span><span className='line-through text-xs text-red-500'>{} ₦{item.price}</span></div>
+                          <div>
+                            <button 
+                              onClick= {()=>handleAddtoCart(item)}
+                              className='mx-1 px-5 py-2 font-medium text-gray-600 focus:outline-none cursor-pointer rounded-lg border border-gray-400 bg-amber-400 hover:bg-amber-200  focus:z-10 focus:ring-4 focus:ring-gray-100'>
+                                <MdAddShoppingCart />
+                            </button>
+                          </div>
+                       </div>
+
+                  {/*}  <p className='hidden text-black text-sm'>Discount: <span className='text-tertiary font-bold md:font-medium'> {item.discount>0? "Yes": "No"}</span> </p>
+                    <p className=' hidden text-black text-sm'>Added: <span className='text-gray-500'>{moment(item.date).fromNow()}</span> </p>*/}
+                    </div> 
+
+                                      
+                  </div>
+        </>
+      )
+     }
+
     return (
         <>
 
@@ -29,29 +87,13 @@ useEffect (()=>{
          {relatedItems? 
          <>
          <div className='mb-5 p-3'>
-             <h2 className='pt-3 sm:pt-8 pb-5 sm:text-center text-2xl sm:text-3xl text-tertiary'> Other items in the same category...</h2>
-             <div className='flex flex-col w-full sm:flex-row md:justify-center'>
+             <h2 className='pt-3 sm:pt-8 pb-5 sm:text-center text-2xl sm:text-3xl text-tertiary'> You might also like...</h2>
+             <div className='grid grid-cols-2 gap-2 md:w-3/5 md:grid-cols-4 md:gap-3 md:justify-center md:mx-auto'>
                 
                 {relatedItems.map((item)=>(
                     <>
-                    <div   key={`${item._id}`} className='flex flex-row p-2  pl-1 pt-5 m-2 md:my-3 md:p-1 md:flex-col md:border md:border-gray-300 md:w-56 md:h-68 md:rounded-sm'> 
-                                       <div className=' flex-none w-32 md:w-full  '> 
-                                             <Link to={`/products/${item._id}`} className='relative'><img className='h-32 md:min-h-36 md:max-h-38  md:h-full md:w-full p-1 rounded-lg m-auto hover:opacity-70' src={item.imageUrl} />
-                                             <span className="absolute top-0 left-0 m-2 rounded-full bg-red-600 px-2 text-center text-xs font-normal text-white"> {item.popular? "Popular": ""} </span>
-                                             {/*<span className="absolute top-5 left-0 m-2 rounded-full bg-red-600 px-2 text-center text-xs font-normal text-white"> {item.deal? "Good deal": ""} </span>*/}
-                                             </Link>
-                                        </div>
-                                        <div className='ml-5  pt-5 flex-1 md:flex-none w-64 md:w-full mr-10  pr-10 md:w-full mr-2 pr-2 md:pt-2 md:mx-2'>
-                                      
-                                        <Link to={`/products/${item._id}`}> 
-                                          <> <span className='text-tertiary font-bold md:font-medium '>₦{Intl.NumberFormat("en-US").format(item.sellingPrice)}     </span><span className='line-through text-xs text-red-500'>{} ₦{item.price}</span></>
-                                           <p className='text-black mt-2 md:mt-0 md:text-base line-clamp-3 md:line-clamp-3 md:py-1 mx-auto'>{capitalizeTitle(item.name)}</p>
-                                           </Link>   
-                                      {/*}  <p className='hidden text-black text-sm'>Discount: <span className='text-tertiary font-bold md:font-medium'> {item.discount>0? "Yes": "No"}</span> </p>
-                                        <p className=' hidden text-black text-sm'>Added: <span className='text-gray-500'>{moment(item.date).fromNow()}</span> </p>*/}
-                                        </div>                                                           
-                                      </div>
-                                                       <hr className=' col-span-5 h-px bg-gray-400 border-0 md:col-span-0 md:hidden'/></>
+                        <ProductCard item={item}/>
+                    </>
                  ))}
              </div>
          </div>
@@ -75,28 +117,13 @@ useEffect (()=>{
         <div className='mb-5 p-3'>
         <hr className=' col-span-5 h-px bg-gray-400 border-0 md:col-span-0 hidden md:flex'/>
              <h2 className='pt-1 sm:pt-10 pb-5 sm:pb-10 sm:text-center text-2xl sm:text-3xl text-tertiary'> Continue shopping...</h2>
-             <div className='flex flex-col w-full sm:flex-row md:justify-center'>
+             <div className='grid grid-cols-2 gap-2 md:w-3/5 md:grid-cols-4 md:gap-3 md:justify-center md:mx-auto'>
                 
 
-             {otherItems.map((item)=>(<>
-                    <div   key={`${item._id}`} className='flex flex-row p-2  pl-1 pt-5 m-2 md:my-3 md:p-1 md:flex-col md:border md:border-gray-300 md:w-56 md:h-68 md:rounded-sm'> 
-                                       <div className=' flex-none w-32 md:w-full  '> 
-                                             <Link to={`/products/${item._id}`} className='relative'><img className='h-32 md:h-full md:w-full p-1 rounded-lg m-auto hover:opacity-70' src={item.imageUrl} />
-                                             <span className="absolute top-0 left-0 m-2 rounded-full bg-red-600 px-2 text-center text-xs font-normal text-white"> {item.popular? "Popular": ""} </span>
-                                             {/*<span className="absolute top-5 left-0 m-2 rounded-full bg-red-600 px-2 text-center text-xs font-normal text-white"> {item.deal? "Good deal": ""} </span>*/}
-                                             </Link>
-                                        </div>
-                                        <div className='ml-5  pt-5 flex-1 md:flex-none w-64 md:w-full mr-10  pr-10 md:w-full mr-2 pr-2 md:pt-2 md:mx-2'>
-                                      
-                                        <Link to={`/products/${item._id}`}> 
-                                          <> <span className='text-tertiary font-bold md:font-medium '>₦{Intl.NumberFormat("en-US").format(item.sellingPrice)}     </span><span className='line-through text-xs text-red-500'>{} ₦{item.price}</span></>
-                                           <p className='text-black mt-2 md:mt-0 md:text-base line-clamp-3 md:line-clamp-3 md:py-1 mx-auto'>{capitalizeTitle(item.name)}</p>
-                                           </Link>   
-                                      {/*}  <p className='hidden text-black text-sm'>Discount: <span className='text-tertiary font-bold md:font-medium'> {item.discount>0? "Yes": "No"}</span> </p>
-                                        <p className=' hidden text-black text-sm'>Added: <span className='text-gray-500'>{moment(item.date).fromNow()}</span> </p>*/}
-                                        </div> 
-                                      </div>
-                                       <hr className=' col-span-5 h-px bg-gray-400 border-0 md:col-span-0 md:hidden'/></>
+             {otherItems.map((item)=>(
+                <>
+                    <ProductCard item={item}/>    
+                </>
                  ))
 
              }
@@ -115,6 +142,10 @@ useEffect (()=>{
          </>
          }
 
+        {notification.show && (
+        <div className="fixed top-5 right-0 m-4 p-2 bg-gray-100 border border-gray-300 font-semibold text-sm text-gray-600 rounded shadow-md">
+          {notification.message}
+        </div>)}
 
         </div>
         </>
